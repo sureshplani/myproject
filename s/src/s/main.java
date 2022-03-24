bf8572bfc52876811e06eb1a2be75a2c17cbff52
Admin <Admin@Admin-PC>
2022-03-24 21:23:19 +0530

    initial commint

---
+++ s/src/s/main.java
@@ -0,0 +1,39 @@
+package s;
+
+//import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		int arr[][]={{10,20,30,40,50},{60,56,89,9,11}};
+//		System.out.println( arr[0][0]+"  "+arr[1][0]);
+//		System.out.println( arr[0][1]+"  "+arr[1][1]);
+//		System.out.println( arr[0][2]+"  "+arr[1][2]);
+//		System.out.println( arr[0][3]+"  "+arr[1][3]);
+//		System.out.println( arr[0][4]+"  "+arr[1][4]);
+//				
+//				
+				int arr[]= {10,20,30,40,50,10};
+				int key=10;
+				for(int i=0;i<6;i++) {
+					if(key==arr[i]) {
+				
+					System.out.println("possion"+i);
+					}
+				}
+					
+				
+		
+				
+				
+				
+				
+			
+		
+		
+		
+	}
+
+}
