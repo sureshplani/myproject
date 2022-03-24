e277fc860edd24189658c1ede44e113c409039fb
sureshplani <102307871+sureshplani@users.noreply.github.com>
2022-03-24 21:54:17 +0530

    Update main.java

--- s/src/s/main.java
+++ s/src/s/main.java
@@ -7,15 +7,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		int arr[][]={{10,20,30,40,50},{60,56,89,9,11}};
-//		System.out.println( arr[0][0]+"  "+arr[1][0]);
-//		System.out.println( arr[0][1]+"  "+arr[1][1]);
-//		System.out.println( arr[0][2]+"  "+arr[1][2]);
-//		System.out.println( arr[0][3]+"  "+arr[1][3]);
-//		System.out.println( arr[0][4]+"  "+arr[1][4]);
-//				
-//				
-				int arr[]= {10,20,30,40,50,10};
+			int arr[]= {10,20,30,40,50,10};
 				int key=10;
 				for(int i=0;i<6;i++) {
 					if(key==arr[i]) {
@@ -24,16 +16,5 @@ public class main {
 					}
 				}
 					
-				
-		
-				
-				
-				
-				
-			
-		
-		
-		
-	}
-
+				}
 }
